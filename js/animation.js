e36a477403e19826b8e35d6445bbf2b48c6ad616
musicis3965 <bobby.timmons@rocketmail.com>
2018-12-05 14:12:06 -0500

    updates

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,11 @@
 
-$(document).ready( () => {
-    TweenMax.from( ".i7", 5, { drawSVG: "0%"});
-});
+// $(document).ready( () => {
+//     // TweenMax.from( ".i7", 5, { drawSVG: "0%"});
+//     TweenMax.from(".i7", 5, {opacity:0, scale:0, ease:Bounce.easeOut});
+// });
+
+// TweenMax.from(".i7", 5, {opacity:0, scale:0, ease:Bounce.easeOut});
+
+TweenMax.staggerFrom(".i7", 2, {opacity:0, y:-200, rotation:360, scale:2, 
+    //delay:0.5
+ });
